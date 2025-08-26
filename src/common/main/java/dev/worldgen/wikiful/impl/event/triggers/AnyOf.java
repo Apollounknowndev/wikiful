313a42a7e45cba45ab8ef0c5bbb8526a865400d2
Apollo <102649729+Apollounknowndev@users.noreply.github.com>
2025-08-26 02:39:11 -0400

    Add template datapack, fix imports

--- src/common/main/java/dev/worldgen/wikiful/impl/event/triggers/AnyOf.java
+++ src/common/main/java/dev/worldgen/wikiful/impl/event/triggers/AnyOf.java
@@ -5,16 +5,8 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.worldgen.wikiful.api.event.EventTrigger;
 import dev.worldgen.wikiful.api.event.EventTriggerType;
 import dev.worldgen.wikiful.impl.registry.WikifulEventTriggerTypes;
-import net.minecraft.advancements.critereon.EntityPredicate;
-import net.minecraft.advancements.critereon.LocationPredicate;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 
 import java.util.List;
-import java.util.Optional;
 
 public record AnyOf(List<EventTrigger> triggers) implements EventTrigger {
     public static final MapCodec<AnyOf> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
